536d16c7a40116490d7add2357991509edb03ce1
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-09-17 16:05:22 +0500

    Regression implementation

--- 14-prediction_on_poly_regression.py
+++ 14-prediction_on_poly_regression.py
@@ -10,18 +10,6 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 df = pd.read_csv("non_linear.csv")
 
-print("Dataset shape:", df.shape)
-print("\nFirst 5 rows:")
-print(df.head())
-
-# Visualize the non-linear relationship
-plt.scatter(df['YearsExperience'], df['Salary'], color='blue', alpha=0.7)
-plt.xlabel("Years of Experience")
-plt.ylabel("Salary ($)")
-plt.title("Experience vs Salary (Non-linear Relationship)")
-plt.grid(True, alpha=0.3)
-plt.show()
-
 
 X = df[["YearsExperience"]]  # Feature
 y = df["Salary"]             # Target
@@ -38,11 +26,6 @@ X_poly_train = poly.fit_transform(X_train)
 X_poly_test = poly.transform(X_test)
 
 
-
-print("Original features shape:", X_train.shape)
-print("Polynomial features shape:", X_poly_train.shape)
-
-
 # Train on polynomial features
 poly_model = LinearRegression()
 poly_model.fit(X_poly_train, y_train)
@@ -56,3 +39,10 @@ poly_r2 = r2_score(y_test, y_hat_poly)
 
 print(poly_mse)
 print(poly_r2)
+
+
+# Predict new value
+new_exp = [[1]]
+new_exp_poly = poly.transform(new_exp)
+pred = poly_model.predict(new_exp_poly)
+print("Predicted salary for 15 years: ",pred )
--- 15-multiple_poly_regression.py
+++ 15-multiple_poly_regression.py
@@ -2,29 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
-df = pd.read_csv("non_linear.csv")
 
-print("Dataset shape:", df.shape)
-print("\nFirst 5 rows:")
-print(df.head())
-
-# Visualize the non-linear relationship
-plt.scatter(df['YearsExperience'], df['Salary'], color='blue', alpha=0.7)
-plt.xlabel("Years of Experience")
-plt.ylabel("Salary ($)")
-plt.title("Experience vs Salary (Non-linear Relationship)")
-plt.grid(True, alpha=0.3)
-plt.show()
+df = pd.read_csv("house_prices.csv")
+print(df)
 
 
-X = df[["YearsExperience"]]  # Feature
-y = df["Salary"]             # Target
+# Define features (all except Price)
+feature_columns = ['Size', 'Bedrooms', 'Bathrooms', 'Age']
+X = df[feature_columns]
+y = df['Price']
+
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(
@@ -32,27 +26,32 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
+
 # Create polynomial features
-poly = PolynomialFeatures(degree=1)
+poly = PolynomialFeatures(degree=2, include_bias=False, interaction_only=False)
 X_poly_train = poly.fit_transform(X_train)
 X_poly_test = poly.transform(X_test)
+print("Model Trained")
 
 
-
-print("Original features shape:", X_train.shape)
-print("Polynomial features shape:", X_poly_train.shape)
-
-
-# Train on polynomial features
+# make model and train it
 poly_model = LinearRegression()
 poly_model.fit(X_poly_train, y_train)
 
 
+
+# Make predictions
 y_hat_poly = poly_model.predict(X_poly_test)
 
 
+# calculate matrix
 poly_mse = mean_squared_error(y_test, y_hat_poly)
+poly_rmse = np.sqrt(poly_mse)
 poly_r2 = r2_score(y_test, y_hat_poly)
 
 print(poly_mse)
+print(poly_rmse)
 print(poly_r2)
+
+
+
